df64e14f451d2bfa58bdd07fd66e585ebf01494d
Dominik <dono3108@gmail.com>
2022-11-23 13:59:38 +0100

    add review and review form to product detail site

--- src/app/modules/product-details/product-details.service.ts
+++ src/app/modules/product-details/product-details.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductDetails } from './model/productDetails';
+import { Review } from './model/review';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class ProductDetailsService {
   constructor(private http: HttpClient) { }
 
   getProductDetail(slug: string): Observable<ProductDetails> {
-    return this.http.get<ProductDetails>("/api/products/" + slug)
+    return this.http.get<ProductDetails>("/api/products/" + slug);
+  }
+
+  saveProductReview(review: Review): Observable<Review> {
+    return this.http.post<Review>("/api/reviews", review);
   }
 }
